3d7625a7282b30aaacd4dadea0b83d747456ee69
kihyeon-kim <enki.heart.kim@gmail.com>
2020-04-07 03:39:58 +0900

    ch3. 3-3

--- src/main/kotlin/com/kihyeon/kotlin/ch3/Ch3.kt
+++ src/main/kotlin/com/kihyeon/kotlin/ch3/Ch3.kt
@@ -179,3 +179,254 @@ fun `exercise-3-6`() {
             }
         }
 }
+
+// 3.3.7
+fun closure() {
+    val taxRate = 0.09
+    fun addTax(price: Double) = price + price * taxRate // addTax 함수는 taxRate 라는 변수에 대해 닫혀있다
+    // 읽기 어렵고 유지보수 하기 어려운 프로그램이 생길 수 있다
+    // 더 모듈화를 하면 읽기 쉽고 유지보수하기 쉬운 프로그램을 만들 수 있다
+
+    fun addTax2(taxRate: Double, price: Double) = price + price * taxRate
+
+    val addTax3 = { taxRateParam: Double, price: Double -> price + price * taxRateParam }
+
+    val addTaxCurr: (Double) -> (Double) -> Double = { taxRateParam: Double ->
+        { price: Double ->
+            price + price * taxRateParam
+        }
+    }
+
+    println(addTaxCurr(taxRate)(12.0))
+}
+
+// 3.3.8
+class TaxComputer(private val rate: Double) {
+    fun compute(price: Double) = price + price * rate
+
+    fun test1() {
+        val tc9 = TaxComputer(0.09)
+        val price = tc9.compute(12.0)
+    }
+
+    val addTaxCurr: (Double) -> (Double) -> Double =
+        { taxRateParam: Double ->
+            { price: Double ->
+                price + price * taxRateParam
+            }
+        }
+
+    fun test2() {
+        val tc9 = addTaxCurr(0.09)
+        val price = tc9(12.0)
+    }
+
+    // 커링은 튜플을 인자로 받는 함수를 한 번에 하나씩 인자를 부분 적용할 수 있는 함수로 대치하는 과정
+    // 커리한 함수를 사용하면 부분 적용이 아주 단순하다는 장점이 있다
+}
+
+fun `exercise-3-7`() {
+    fun <A, B, C> partialA(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
+}
+
+fun `exercise-3-8`() {
+    fun <A, B, C> aaa(b: B, f: (A) -> (B) -> C): (A) -> C =
+        { a: A ->
+            f(a)(b)
+        }
+}
+
+fun `exercise-3-9`() {
+    fun <A, B, C, D> func(a: A, b: B, c: C, d: D): String = "$a, $b, $c, $d"
+
+    fun <A, B, C, D> funcCurr(): (A) -> (B) -> (C) -> (D) -> String =
+        { a: A ->
+            { b: B ->
+                { c: C ->
+                    { d: D ->
+                        "$a, $b, $c, $d"
+                    }
+                }
+            }
+        }
+
+    //    val a = { a: A ->  << XXX. 클래스나 인스턴스 또는 fun 함수를 정의해야한다
+    //    }
+}
+
+fun `exercise-3-10`() {
+    // ??
+    fun <A, B, C> source(c: C): (A, B) -> C =
+        { a: A, b: B ->
+            c
+        }
+
+    fun <A, B, C> curr(f: (A, B) -> C): (A) -> (B) -> C =
+        { a: A ->
+            { b: B ->
+                f(a, b)
+            }
+        }
+
+    val a = curr<Int, Int, Int>(source(1))
+}
+
+fun `exercise-3-11`() {
+    fun <A, B, C> swap(f: (A) -> (B) -> C): (B) -> (A) -> C =
+        { b: B ->
+            { a: A ->
+                f(a)(b)
+            }
+        }
+}
+
+// 3.3.11. 올바른 타입 사용하기
+
+data class Product(val name: String, val price: Double, val weight: Double)
+
+data class OrderLine(val product: Product, val count: Int) {
+    fun weight() = product.weight * count
+    fun amount() = product.price * count
+}
+
+object Store {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val toothPaste = Product("Tooth paste", 1.5, 0.5)
+        val toothBrush = Product("Tooth brush", 3.5, 0.3)
+        val orderLines = listOf(
+            OrderLine(toothPaste, 2),
+            OrderLine(toothBrush, 3)
+        )
+
+        val weight = orderLines.sumByDouble { it.amount() } // <<< z
+        val price = orderLines.sumByDouble { it.weight() }
+
+        println("Total price: $price")
+        println("Total weight: $weight")
+
+        // 오류는 당연하지만, 컴파일러가 경고 하지 않았다. 너무 흔히 쓰는 타입을 사용했음.
+        // >> 값 타입(value type) 을 쓰자!. 값을 표현하는 타입.
+    }
+}
+
+//data class Price(val value: Double)
+//
+//data class Weight(val value: Double)
+
+val total = Price(1.0).value + Weight(1.0).value // << 이러면 문제. plus 를 정의.
+
+data class Price(val value: Double) {
+    operator fun plus(price: Price): Price = Price(this.value + price.value)
+
+    operator fun times(num: Int): Price = Price(this.value * num)
+}
+
+data class Weight(val value: Double) {
+    operator fun plus(weight: Weight): Weight = Weight(this.value + weight.value)
+
+    operator fun times(num: Int): Weight = Weight(this.value * num)
+}
+
+val totalPrice: Price = Price(1.0) + Price(2.0)
+
+// 컬렉션을 하나의 값으로 줄여 나가는 과정. fold. reduce.
+// 시작하는 원소를 제공하느냐(fold), 제공하지 않느냐(reduce)
+// 결과 타입이 컬렉션의 원소 타입과 동일하냐(reduce), 다르냐(fold)
+
+
+class ValueType {
+    data class Product(val name: String, val price: Price, val weight: Weight)
+
+    data class OrderLine(val product: Product, val count: Int) {
+        fun weight() = product.weight * count
+        fun amount() = product.price * count
+    }
+
+    object Store {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val zeroPrice = Price(0.0)
+            val zeroWeight = Weight(0.0)
+            //val priceAddition = { x, y -> x + y }
+
+            val toothPaste = Product("Tooth paste", Price(1.5), Weight(0.5))
+            val toothBrush = Product("Tooth brush", Price(3.5), Weight(0.3))
+
+            @Suppress()
+            val orderLines = listOf(
+                OrderLine(toothPaste, 2),
+                OrderLine(toothBrush, 3)
+            )
+
+            val weight: Weight = orderLines.fold(zeroWeight) { a, b -> a + b.weight() }
+            val price: Price = orderLines.fold(zeroPrice) { a, b -> a + b.amount() }
+
+            println("Total price: $price")
+            println("Total weight: $weight")
+        }
+    }
+}
+
+class FactoryIdentity {
+    data class Price private constructor(private val value: Double) {
+        override fun toString(): String = value.toString()
+        operator fun plus(price: Price) = Price(this.value + price.value)
+        operator fun times(num: Int) = Price(this.value * num)
+
+        companion object {
+            val identity = Price(0.0)
+            operator fun invoke(value: Double) =
+                if (value > 0) {
+                    Price(value)
+                } else {
+                    throw IllegalArgumentException("Price must be positive or null")
+                }
+        }
+    }
+
+    data class Weight private constructor(private val value: Double) {
+        override fun toString(): String = value.toString()
+        operator fun plus(weight: Weight) = Weight(this.value + weight.value)
+        operator fun times(num: Int) = Weight(this.value * num)
+
+        companion object {
+            val identity = Weight(0.0)
+            operator fun invoke(value: Double): Weight =
+                if (value > 0) {
+                    Weight(value)
+                } else {
+                    throw IllegalArgumentException("Weight must be positive or null")
+                }
+        }
+    }
+
+    data class Product(val name: String, val price: Price, val weight: Weight)
+
+    data class OrderLine(val product: Product, val count: Int) {
+        fun weight() = product.weight * count
+        fun amount() = product.price * count
+    }
+
+    object Store {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //val priceAddition = { x, y -> x + y }
+
+            val toothPaste = Product("Tooth paste", Price(1.5), Weight(0.5))
+            val toothBrush = Product("Tooth brush", Price(3.5), Weight(0.3))
+
+            @Suppress()
+            val orderLines = listOf(
+                OrderLine(toothPaste, 2),
+                OrderLine(toothBrush, 3)
+            )
+
+            val weight: Weight = orderLines.fold(Weight.identity) { a, b -> a + b.weight() }
+            val price: Price = orderLines.fold(Price.identity) { a, b -> a + b.amount() }
+
+            println("Total price: $price")
+            println("Total weight: $weight")
+        }
+    }
+}
